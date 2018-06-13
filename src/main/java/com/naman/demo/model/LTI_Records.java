348e8c8acf63fb482d3a4872931342d770add3dd
Ankit Kumar <2012ankitkmr@gmail.com>
2018-06-13 11:57:46 +0530

    Update LTI_Records.java

--- src/main/java/com/naman/demo/model/LTI_Records.java
+++ src/main/java/com/naman/demo/model/LTI_Records.java
@@ -1,11 +1,12 @@
 package com.naman.demo.model;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class LTI_Records {
 	
-	
+	@Id
 	private String name;
 	private String cloudgroups;
 	
